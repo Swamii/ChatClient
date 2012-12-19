de5697db89b6be4c69e39f9270055630b9ccf000
D.Borgenstedt <daniel.borgenstedt@gmail.com>
2012-12-19 14:19:20 +0100

    fyllt på med skit

--- src/Client.java
+++ src/Client.java
@@ -21,22 +21,47 @@ public class Client {
 		output = new BufferedWriter( new OutputStreamWriter(socket.getOutputStream()) );
 		System.out.println("Streams are good to go!");
 	}
+
+	public void showText(final String message){
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				gui.chatWindow.append(message);				//append gör att det skrivs till efter nuvarande
+			}
+		});
+	}
 	
 	public void chatting(){
 		do{
 			try{
-				message = input.readLine();
-				showText("\n " + message);
+				message = input.readLine();					//Läser in raden som kommer från server
+				if(message.startsWith("MESSAGE:")){
+					String mess = message.split(":")[1];
+					showText("\n " + mess); 				//showText-metoden skickar upp meddelandet på chattfältet
+				}
+			
 			}catch(IOException notfound){
 				showText("\n Error on recieve..." );
 			}
-		}while(!message.equals("END"));
+		}while(!message.equals("END"));						//Om man skriver END så avslutas chatten
 	}
 	
-	public void showText(final String mess){
-		SwingUtilities.invokeLater(arg0)
+	
+	public void sendMessage(String message){
+		try{
+			output.write("MESSAGE:"+message);				//Skickar ett meddelande
+			output.flush();
+		}catch (IOException e){
+			gui.chatWindow.append("Can't send message...");
+		}
 	}
+	
 	public void close(){
-
+		try {
+			input.close();
+			output.close();
+			socket.close();
+		} catch (IOException e) {
+			System.out.println("Failed to close.");
+		}
 	}
 }
