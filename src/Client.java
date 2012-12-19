d6d306ca0f096239b8e4460c85159a84c0a491c6
D.Borgenstedt <daniel.borgenstedt@gmail.com>
2012-12-19 14:40:21 +0100

    blaj

--- src/Client.java
+++ src/Client.java
@@ -25,7 +25,7 @@ public class Client {
 	public void showText(final String message){
 		SwingUtilities.invokeLater(new Runnable(){
 			public void run(){
-				gui.chatWindow.append(message);				//append gör att det skrivs till efter nuvarande
+				gui.chatWindow.append(message);				//append gör att det skrivs till efter nuvarande text
 			}
 		});
 	}
@@ -34,9 +34,12 @@ public class Client {
 		do{
 			try{
 				message = input.readLine();					//Läser in raden som kommer från server
-				if(message.startsWith("MESSAGE:")){
-					String mess = message.split(":")[1];
+				if(message.startsWith("MESSAGE:")){			//Servern skickar med en string specifikt för chatt
+					String mess = message.split(":")[1];	//Delar på meddelandet efter :
 					showText("\n " + mess); 				//showText-metoden skickar upp meddelandet på chattfältet
+				}else if(message.startsWith("NEW USER:")){
+					String mess = message.split(":")[1];	
+					showText("\n " + mess); 
 				}
 			
 			}catch(IOException notfound){
@@ -49,9 +52,9 @@ public class Client {
 	public void sendMessage(String message){
 		try{
 			output.write("MESSAGE:"+message);				//Skickar ett meddelande
-			output.flush();
+			output.flush();									//Spolar toaletten :)
 		}catch (IOException e){
-			gui.chatWindow.append("Can't send message...");
+			gui.chatWindow.append("Can't send message..."); //Lägger upp texten i chattfältet 
 		}
 	}
 	
